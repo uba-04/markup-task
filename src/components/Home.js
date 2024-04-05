33d10240fefef96ee778aba297902ae6bab01ede
Ubaldo Flor <ubaldo_flor@epam.com>
2024-04-04 20:18:27 -0600

    Implement routing

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Breadcrumb from './Breadcrumb';
+
+function Home() {
+
+    return (
+        <>
+            <Breadcrumb path="/"/>
+            <p>This is the Home Page.</p>
+        </>
+    );
+}
+
+export default Home;
